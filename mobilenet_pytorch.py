b7080ba1cb3b3c8bd76d514afc3f4c05808e2ce5
Irene Papadopoulou <irenepap3@gmail.com>
2022-01-20 19:00:48 +0100

    Added pytorch and tf implementations of classifier.

---
+++ mobilenet_pytorch.py
@@ -0,0 +1,127 @@
+import torch.nn as nn
+import torch
+from torchsummary import summary
+import numpy as np
+
+import tensorflow as tf
+import cv2
+from matplotlib import pyplot as plt
+
+
+class MobileNetV1(nn.Module):
+    def __init__(self):
+        super(MobileNetV1, self).__init__()
+
+        def conv_bn(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, oup, 3, stride, padding=1, bias=False),
+                # nn.BatchNorm2d(oup),
+                # nn.ReLU(inplace=True)
+            )
+
+        def conv_dw(inp, oup, stride):
+            return nn.Sequential(
+                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+                nn.BatchNorm2d(inp),
+                nn.ReLU(inplace=True),
+    
+                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+                nn.ReLU(inplace=True),
+            )
+
+        self.model = nn.Sequential(
+            conv_bn(3, 32, 2),
+            # conv_dw(32, 64, 1),
+            # conv_dw(64, 128, 2),
+            # conv_dw(128, 128, 1),
+            # conv_dw(128, 256, 2),
+            # conv_dw(256, 256, 1),
+            # conv_dw(256, 512, 2),
+            # conv_dw(512, 512, 1),
+            # conv_dw(512, 512, 1),
+            # conv_dw(512, 512, 1),
+            # conv_dw(512, 512, 1),
+            # conv_dw(512, 512, 1),
+            # conv_dw(512, 1024, 2),
+            # conv_dw(1024, 1024, 1),
+            # # nn.AdaptiveAvgPool2d(1),
+            # nn.AvgPool2d(7),
+            # nn.Dropout(),
+            # nn.Conv2d(1024, 2, 1, 1, 0)
+        )
+        #self.fc = nn.Linear(1024, 2)
+
+    def forward(self, x):
+        x = self.model(x)
+        # x = x.view(-1, 2)
+        # x = self.fc(x)
+        return x
+
+
+def convert_tf_classifier(classifier):
+    model = MobileNetV1()
+
+    pretrained_params = vars(classifier)["_serialized_attributes"]["trainable_variables"]
+    total_params = vars(classifier)["_serialized_attributes"]["variables"]
+
+    moving_means = [i.numpy() for i in total_params if "moving_mean" in i.name]
+    moving_vars = [i.numpy() for i in total_params if "moving_variance" in i.name]
+    
+    for i, (name, param) in enumerate(model.named_parameters()):
+        # print(name, i)
+        block_idx = name.split(".")[1]
+        inter_block_idx = name.split(".")[2]
+
+        pre_param = pretrained_params[i].numpy()
+        
+        if len(pre_param.shape) == 4: # Conv layer
+            if block_idx in ["0", "16"] or inter_block_idx == "3":
+                pre_param = pre_param.transpose(3,2,0,1)
+            else: # inter_block_idx == "0"
+                pre_param = pre_param.transpose(2,3,0,1)
+
+        # print(pretrained_params[i].name)
+
+        param.data = torch.from_numpy(pre_param)
+
+        if inter_block_idx in ["1", "4"] and "bias" in name:
+            bn_module = model.model[int(block_idx)][int(inter_block_idx)]
+            bn_module.running_mean = torch.from_numpy(moving_means.pop(0))
+            bn_module.running_var = torch.from_numpy(moving_vars.pop(0))
+
+        # print(pre_param.shape)
+        # print("Torch: ", param.data.dtype)
+        # print(pretrained_params[i].numpy().shape)
+        # print("TF: ", pretrained_params[i].numpy())
+
+    return model
+
+if __name__ == '__main__':
+    input = cv2.imread('imgs/2.png')
+    input = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)[None, ...]/255
+
+    classifier = tf.keras.models.load_model('./mobilenet.savedmodel')
+
+    # print('theirs', classifier.predict(input))
+
+    #load model pretrained model weights
+    model = convert_tf_classifier(classifier)
+
+    summary(model, (3, 256, 256))
+
+    #check weights of first layer
+    print('Weights of first layer:')
+    print(model.model[0][0].weight)
+
+    # torch needs input in form [B, C, H, W] but input is [B, H, W, C]
+    input = torch.tensor(input.transpose(0,3,1,2), dtype=torch.float32)
+
+    model.eval()
+    output = model.forward(input)
+
+    # print('theirs', classifier.predict(input.numpy().transpose(0,2,3,1)))
+    # output = output.permute(0,2,3,1)
+    # print(np.round(output.detach().numpy(),4))
+    print(output.shape)
+    print('ours', output)
---
+++ mobilenet_tf.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, DepthwiseConv2D
+from tensorflow.keras.layers import Conv2D, BatchNormalization
+from tensorflow.keras.layers import ReLU, AvgPool2D, Flatten, Dense
+from tensorflow.keras import Model
+from tensorflow.keras.layers import GlobalAveragePooling2D, Reshape, Dropout, Activation
+import cv2
+
+
+def mobilnet_block (x, filters, strides):
+    
+    x = DepthwiseConv2D(kernel_size = 3, strides = strides, padding = 'same', use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    
+    x = Conv2D(filters = filters, kernel_size = 1, strides = 1, use_bias=False)(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    
+    return x
+
+input = Input(shape = (256,256,3))
+x = Conv2D(filters = 32, kernel_size = 3, strides = 2, padding = 'same', use_bias=False)(input)
+# x = BatchNormalization()(x)
+# x = ReLU()(x)
+
+# x = mobilnet_block(x, filters = 64, strides = 1)
+# x = mobilnet_block(x, filters = 128, strides = 2)
+# x = mobilnet_block(x, filters = 128, strides = 1)
+# x = mobilnet_block(x, filters = 256, strides = 2)
+# x = mobilnet_block(x, filters = 256, strides = 1)
+# x = mobilnet_block(x, filters = 512, strides = 2)
+
+# for _ in range (5):
+#      x = mobilnet_block(x, filters = 512, strides = 1)
+# x = mobilnet_block(x, filters = 1024, strides = 2)
+# x = mobilnet_block(x, filters = 1024, strides = 1)
+
+# x = GlobalAveragePooling2D()(x)
+# x = Reshape((1,1,1024))(x)
+# x = Dropout(0.2)(x)
+# x = Conv2D(filters=2, kernel_size=1)(x)
+# x = Reshape((2,))(x)
+
+# output = Activation(tf.nn.tanh)(x)
+
+model = Model(inputs=input, outputs=x)
+
+input = cv2.imread('imgs/2.png')
+input = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)[None, ...]/255
+
+classifier = tf.keras.models.load_model('./mobilenet.savedmodel')
+
+#load model pretrained model weights
+weight_count = len(model.get_weights())
+model.set_weights(classifier.get_weights()[:weight_count])
+
+model.summary()
+
+print('Weights of first layer:')
+# we transpose to compare if they are the same
+print(model.get_weights()[0].transpose(3,2,0,1))
+
+# print('theirs', classifier.predict(input))
+print('ours', model.predict(input))
+
