1a37fb0bab5d69b55c3270f6e851d0663e56091f
VicK9 <40694059+VicK9@users.noreply.github.com>
2022-02-02 21:16:21 +0100

    Added the encoder into the notebook

--- legacy_modified.py
+++ legacy_modified.py
@@ -5,7 +5,7 @@
 # and any modifications thereto.  Any use, reproduction, disclosure or
 # distribution of this software and related documentation without an express
 # license agreement from NVIDIA CORPORATION is strictly prohibited.
-
+import tensorflow
 import click
 import pickle
 import re
@@ -14,7 +14,6 @@ import torch
 import stylegan2_pytorch.dnnlib as dnnlib
 from stylegan2_pytorch.training.torch_utils import misc
 from stylegan2_pytorch.training import networks
-
 import collections
 
 from os import path
@@ -24,6 +23,127 @@ from PIL import ImageDraw
 from PIL import ImageFont
 from io import BytesIO
 import IPython.display
+
+
+def create_dlat_from_img_and_logits(E, logits, image):
+    image = torch.from_numpy(image).unsqueeze(0)
+    enc_out = E(image,2)
+    dlatent = torch.cat([enc_out, logits], dim=1)
+    return dlatent
+#----------------------------------------------------------------------------
+def load_torch_encoder(pkl_file_path='./models/encoder/encoder_kwargs.pkl', pth_file='./models/encoder/encoder.pth'):
+    
+    print('Loading encoder\'s necessary kwargs...')
+    with open(pkl_file_path, 'rb') as f:
+        kwargs = pickle.load(f)
+    print('Creating encoder model...')
+    E = networks.Encoder(**kwargs).eval().requires_grad_(False)
+    print('Loading encoder\'s state dict...')
+    E.load_state_dict(torch.load(pth_file))
+    print('Done')
+    return E
+
+#----------------------------------------------------------------------------
+
+def convert_encoder_tfpkl_to_pytorch(source_path='saved_e_dictionary.pkl', dest_path='./models/encoder/encoder.pth'):
+
+    print(f'Loading "{source_path}"...')
+    with open(source_path, 'rb') as f:
+        tf_net = pickle.load(f)
+    
+    print(f'Converting network from Tensorflow to Pytorch...')
+    torch_model = convert_tf_encoder(tf_net)
+    
+    print(f'Saving "{dest_path}"...')
+    torch.save(torch_model.state_dict(), dest_path)
+    print(f'Done!')
+    
+    return torch_model
+#----------------------------------------------------------------------------
+def convert_tf_encoder(tf_E):
+   
+    # Collect kwargs.
+    tf_kwargs = {}
+    known_kwargs = set()
+    def kwarg(tf_name, default=None):
+        known_kwargs.add(tf_name)
+        return tf_kwargs.get(tf_name, default)
+
+    # Convert kwargs.
+    kwargs = dnnlib.EasyDict(
+        c_dim                   = kwarg('label_size',           0), #from 0 to 2
+        img_resolution          = kwarg('resolution',           256), #from 1024 to 256
+        img_channels            = kwarg('num_channels',         3),
+        architecture            = kwarg('architecture',         'resnet'),
+        channel_base            = kwarg('fmap_base',            16384) * 2,
+        channel_max             = kwarg('fmap_max',             512),
+        num_fp16_res            = kwarg('num_fp16_res',         0),
+        conv_clamp              = kwarg('conv_clamp',           None),
+        cmap_dim                = kwarg('mapping_fmaps',        None),
+        block_kwargs = dnnlib.EasyDict(
+            activation          = kwarg('nonlinearity',         'lrelu'),
+            resample_filter     = kwarg('resample_kernel',      [1,3,3,1]),
+            freeze_layers       = kwarg('freeze_layers',        0),
+        ),
+        mapping_kwargs = dnnlib.EasyDict(
+            num_layers          = kwarg('mapping_layers',       0),
+            embed_features      = kwarg('mapping_fmaps',        None),
+            layer_features      = kwarg('mapping_fmaps',        None),
+            activation          = kwarg('nonlinearity',         'lrelu'),
+            lr_multiplier       = kwarg('mapping_lrmul',        0.1),
+        ),
+        epilogue_kwargs = dnnlib.EasyDict(
+            mbstd_group_size    = kwarg('mbstd_group_size',     None),
+            mbstd_num_channels  = kwarg('mbstd_num_features',   0),
+            activation          = kwarg('nonlinearity',         'lrelu'),
+        ),
+    )
+
+    # Check for unknown kwargs.
+    kwarg('structure')
+    unknown_kwargs = list(set(tf_kwargs.keys()) - known_kwargs)
+    if len(unknown_kwargs) > 0:
+        raise ValueError('Unknown TensorFlow kwarg', unknown_kwargs[0])
+
+    # Save kwargs of the generator
+    with open('./models/encoder/encoder_kwargs.pkl', 'wb') as f:
+        pickle.dump(kwargs, f)
+    # Collect params.
+    tf_params = _collect_tf_params_mod(tf_E)
+    for name, value in list(tf_params.items()):
+        match = re.fullmatch(r'FromRGB_lod(\d+)/(.*)', name)
+        if match:
+            r = kwargs.img_resolution // (2 ** int(match.group(1)))
+            tf_params[f'{r}x{r}/FromRGB/{match.group(2)}'] = value
+            kwargs.architecture = 'orig'
+    #for name, value in tf_params.items(): print(f'{name:<50s}{list(value.shape)}')
+
+    # Convert params.
+    E = networks.Encoder(**kwargs).eval().requires_grad_(False)
+
+    
+    _populate_module_params(E,
+        r'b(\d+)\.fromrgb\.weight',     lambda r:       np.array(tf_params[f'encoder/EncoderFromRGB{r}x{r}/StyleGAN2Conv2D/weight:0']).transpose(3, 2, 0, 1),
+        r'b(\d+)\.fromrgb\.bias',       lambda r:       np.array(tf_params[f'encoder/EncoderFromRGB{r}x{r}/FusedBiasActivation/FromRGBBias:0']).squeeze(),
+        r'b(\d+)\.conv0\.weight',       lambda r:       np.array(tf_params[f'encoder/EncoderBlock{r}x{r}/Conv0/weight:0']).transpose(3, 2, 0, 1),
+        r'b(\d+)\.conv0\.bias',         lambda r:       np.array(tf_params[f'encoder/EncoderBlock{r}x{r}/Conv0Bias/BiasAct0:0']).squeeze(),
+        r'b(\d+)\.conv1\.weight',       lambda r:       np.array(tf_params[f'encoder/EncoderBlock{r}x{r}/Conv1Down/weight:0']).transpose(3, 2, 0, 1),
+        r'b(\d+)\.conv1\.bias',         lambda r:       np.array(tf_params[f'encoder/EncoderBlock{r}x{r}/Conv1DownBiasAct/BiasAct1:0']).squeeze(),
+        r'b(\d+)\.skip\.weight',        lambda r:       np.array(tf_params[f'encoder/EncoderBlock{r}x{r}/Skip/weight:0']).transpose(3, 2, 0, 1),
+        r'mapping\.embed\.weight',      lambda:         np.array(tf_params[f'LabelEmbed/weight']).transpose(),
+        r'mapping\.embed\.bias',        lambda:         np.array(tf_params[f'LabelEmbed/bias']),
+        r'mapping\.fc(\d+)\.weight',    lambda i:       np.array(tf_params[f'Mapping{i}/weight']).transpose(),
+        r'mapping\.fc(\d+)\.bias',      lambda i:       np.array(tf_params[f'Mapping{i}/bias']),
+        r'b4\.conv\.weight',            lambda:         np.array(tf_params[f'encoder/Conv4x4/weight:0']).transpose(3, 2, 0, 1),
+        r'b4\.conv\.bias',              lambda:         np.array(tf_params[f'encoder/EncoderConvBiasAct4x4/ConvBiasAct4x4:0']).squeeze(),
+        r'b4\.fc\.weight',              lambda:         np.array(tf_params[f'encoder/EncoderDense4x4/weight:0']).transpose(),
+        r'b4\.fc\.bias',                lambda:         np.array(tf_params[f'encoder/EncoderDenseBiasAct4x4/DenseBiasAct4x4:0']).squeeze(),
+        r'b4\.out\.weight',             lambda:         np.array(tf_params[f'encoder/style_gan2_dense/weight:0']).transpose(),
+        r'b4\.out\.bias',               lambda:         np.array(tf_params[f'encoder/EncoderOutputBiasAct/OutputBiasAct:0']).squeeze(),
+        r'.*\.resample_filter',         None,
+    )
+    return E
+
 
 #----------------------------------------------------------------------------
 def load_torch_generator(pkl_file_path='generator_kwargs.pkl', pth_file='generator.pth'):
@@ -218,40 +338,7 @@ def convert_tf_generator(tf_G):
 
     # Convert params.
     G = networks.Generator(**kwargs).eval().requires_grad_(False)
-    # pylint: disable=unnecessary-lambda
-    # _populate_module_params(G,
-    #     r'mapping\.w_avg',                                  lambda:     tf_params[f'dlatent_avg'],
-    #     r'mapping\.embed\.weight',                          lambda:     tf_params[f'mapping/LabelEmbed/weight'].transpose(),
-    #     r'mapping\.embed\.bias',                            lambda:     tf_params[f'mapping/LabelEmbed/bias'],
-    #     r'mapping\.fc(\d+)\.weight',                        lambda i:   tf_params[f'mapping/Dense{i}/weight'].transpose(),
-    #     r'mapping\.fc(\d+)\.bias',                          lambda i:   tf_params[f'mapping/Dense{i}/bias'],
-    #     r'synthesis\.b4\.const',                            lambda:     tf_params[f'synthesis/4x4/Const/const'][0],
-    #     r'synthesis\.b4\.conv1\.weight',                    lambda:     tf_params[f'synthesis/4x4/Conv/weight'].transpose(3, 2, 0, 1),
-    #     r'synthesis\.b4\.conv1\.bias',                      lambda:     tf_params[f'synthesis/4x4/Conv/bias'],
-    #     r'synthesis\.b4\.conv1\.noise_const',               lambda:     tf_params[f'synthesis/noise0'][0, 0],
-    #     r'synthesis\.b4\.conv1\.noise_strength',            lambda:     tf_params[f'synthesis/4x4/Conv/noise_strength'],
-    #     r'synthesis\.b4\.conv1\.affine\.weight',            lambda:     tf_params[f'synthesis/4x4/Conv/mod_weight'].transpose(),
-    #     r'synthesis\.b4\.conv1\.affine\.bias',              lambda:     tf_params[f'synthesis/4x4/Conv/mod_bias'] + 1,
-    #     r'synthesis\.b(\d+)\.conv0\.weight',                lambda r:   tf_params[f'synthesis/{r}x{r}/Conv0_up/weight'][::-1, ::-1].transpose(3, 2, 0, 1),
-    #     r'synthesis\.b(\d+)\.conv0\.bias',                  lambda r:   tf_params[f'synthesis/{r}x{r}/Conv0_up/bias'],
-    #     r'synthesis\.b(\d+)\.conv0\.noise_const',           lambda r:   tf_params[f'synthesis/noise{int(np.log2(int(r)))*2-5}'][0, 0],
-    #     r'synthesis\.b(\d+)\.conv0\.noise_strength',        lambda r:   tf_params[f'synthesis/{r}x{r}/Conv0_up/noise_strength'],
-    #     r'synthesis\.b(\d+)\.conv0\.affine\.weight',        lambda r:   tf_params[f'synthesis/{r}x{r}/Conv0_up/mod_weight'].transpose(),
-    #     r'synthesis\.b(\d+)\.conv0\.affine\.bias',          lambda r:   tf_params[f'synthesis/{r}x{r}/Conv0_up/mod_bias'] + 1,
-    #     r'synthesis\.b(\d+)\.conv1\.weight',                lambda r:   tf_params[f'synthesis/{r}x{r}/Conv1/weight'].transpose(3, 2, 0, 1),
-    #     r'synthesis\.b(\d+)\.conv1\.bias',                  lambda r:   tf_params[f'synthesis/{r}x{r}/Conv1/bias'],
-    #     r'synthesis\.b(\d+)\.conv1\.noise_const',           lambda r:   tf_params[f'synthesis/noise{int(np.log2(int(r)))*2-4}'][0, 0],
-    #     r'synthesis\.b(\d+)\.conv1\.noise_strength',        lambda r:   tf_params[f'synthesis/{r}x{r}/Conv1/noise_strength'],
-    #     r'synthesis\.b(\d+)\.conv1\.affine\.weight',        lambda r:   tf_params[f'synthesis/{r}x{r}/Conv1/mod_weight'].transpose(),
-    #     r'synthesis\.b(\d+)\.conv1\.affine\.bias',          lambda r:   tf_params[f'synthesis/{r}x{r}/Conv1/mod_bias'] + 1,
-    #     r'synthesis\.b(\d+)\.torgb\.weight',                lambda r:   tf_params[f'synthesis/{r}x{r}/ToRGB/weight'].transpose(3, 2, 0, 1),
-    #     r'synthesis\.b(\d+)\.torgb\.bias',                  lambda r:   tf_params[f'synthesis/{r}x{r}/ToRGB/bias'],
-    #     r'synthesis\.b(\d+)\.torgb\.affine\.weight',        lambda r:   tf_params[f'synthesis/{r}x{r}/ToRGB/mod_weight'].transpose(),
-    #     r'synthesis\.b(\d+)\.torgb\.affine\.bias',          lambda r:   tf_params[f'synthesis/{r}x{r}/ToRGB/mod_bias'] + 1,
-    #     r'synthesis\.b(\d+)\.skip\.weight',                 lambda r:   tf_params[f'synthesis/{r}x{r}/Skip/weight'][::-1, ::-1].transpose(3, 2, 0, 1),
-    #     r'.*\.resample_filter',                             None,
-    # )
-    
+
     _populate_module_params(G,
         r'mapping\.w_avg',                                  lambda:     np.array(tf_params[f'dlatent_avg:0']), #done
         r'mapping\.embed\.weight',                          lambda:     np.array(tf_params[f'mapping/LabelEmbed/weight']).transpose(), #?
@@ -376,7 +463,7 @@ def convert_tf_discriminator(tf_D):
 
 @click.command()
 @click.option('--source', help='Input pickle', default='saved_g_dictionary.pkl', required=True, metavar='PATH')
-@click.option('--dest', help='Output pickle', default='generator.pth', required=True, metavar='PATH')
+@click.option('--dest', help='Output pickle', default='./encoder/encoder.pth', required=True, metavar='PATH')
 @click.option('--force-fp16', help='Force the networks to use FP16', type=bool, default=False, metavar='BOOL', show_default=True)
 def convert_network_pickle(source, dest, force_fp16):
     """Convert legacy network pickle into the native PyTorch format.
@@ -391,10 +478,23 @@ def convert_network_pickle(source, dest, force_fp16):
         --source=https://nvlabs-fi-cdn.nvidia.com/stylegan2/networks/stylegan2-cat-config-f.pkl \\
         --dest=stylegan2-cat-config-f.pkl
     """
-    if not path.exists(dest):
-        G = convert_tfpkl_to_pytorch(source,dest)
+    # if not path.exists(dest):
+    #     G = convert_tfpkl_to_pytorch(source,dest)
         
-    G = load_torch_generator()
+    # G = load_torch_generator()
+    # create_images_from_dlatent(G)
+    E = convert_encoder_tfpkl_to_pytorch()
+    E = load_torch_encoder()
+    # G = load_torch_generator()
+    # CLASSIFIER_PATH = './models/classifier.pth'
+    # classifier = MobileNetV1()
+    # classifier.load_state_dict(torch.load(CLASSIFIER_PATH))
+    # classifier.eval()
+    # classifier.requires_grad_(False)
+    # image = plt.imread('69931_256.png').transpose(2,0,1)
+    # logits = classifier(torch.from_numpy(image).unsqueeze(0))
+    # dlat = create_dlat_from_img(E, logits, image)
+    # create_images_from_my_dlatent(G,dlatent=dlat)
 
 #----------------------------------------------------------------------------
 
